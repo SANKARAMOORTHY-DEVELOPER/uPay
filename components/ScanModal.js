548ba5e8d0c11f78b0b5cf87774f11b69e9ba919
Adithya2310 <Adithyanandi099@gmail.com>
2023-10-20 23:24:59 +0530

    Qr Code is working

--- components/ScanModal.js
+++ components/ScanModal.js
@@ -47,17 +47,15 @@ const ScanModal = () => {
 
   const [result, setResult] = useState(null);
   // to store the object
-  const [qrResult,setQrResult]=useState(null);
 
   const handleScan = (scan) => {
       console.log("Scan",scan);
-      setResult(scan.data);
-      console.log("Result",result);
+      setResult(()=>{
       // Define a regular expression pattern to match paymentId and account
-      const pattern = /paymentId=([^\$]*)\$account=([^\$]*)\$USD=([^\$]*)\$token=([^\$]*)\$price=(.*)/;
-
+      const pattern = /paymentid=([^\$]*)\$account=([^\$]*)\$USD=([^\$]*)\$token=([^\$]*)\$price=(.*)/;
+      console.log(scan.data);
       // Use the `match` method to extract the values
-      const matches = result.match(pattern);
+      const matches = scan.data.match(pattern);
 
       const data={
         paymentId:matches[1],
@@ -66,14 +64,36 @@ const ScanModal = () => {
         token:matches[4],
         amount:matches[5]
       }
-      setQrResult(data);
-      console.log("setQrResult",data);
     
       if (matches) {
         console.log("Account:", data);
       } else {
         console.log("No match found in the string.");
       }
+      return data;
+      });
+      console.log("Result",result);
+      // // Define a regular expression pattern to match paymentId and account
+      // const pattern = /paymentId=([^\$]*)\$account=([^\$]*)\$USD=([^\$]*)\$token=([^\$]*)\$price=(.*)/;
+
+      // // Use the `match` method to extract the values
+      // const matches = result.match(pattern);
+
+      // const data={
+      //   paymentId:matches[1],
+      //   account:matches[2],
+      //   tokenPrice:matches[3],
+      //   token:matches[4],
+      //   amount:matches[5]
+      // }
+      // setQrResult(data);
+      // console.log("setQrResult",data);
+    
+      // if (matches) {
+      //   console.log("Account:", data);
+      // } else {
+      //   console.log("No match found in the string.");
+      // }
       // console.log("scan result",result);
   };
 
@@ -97,8 +117,7 @@ const ScanModal = () => {
   {
     return (
       <div className="max-w-xs rounded overflow-hidden shadow-lg m-4">
-      HI
-      {/* <h1 className="text-3xl font-semibold text-center">Pay</h1>
+      <h1 className="text-3xl font-semibold text-center">Pay</h1>
       <div className="px-6 py-4">
         <p className=" text-gray-600">Payment ID: <span className=" text-black">{result.paymentId}</span></p>
         <p className=" text-gray-600">Account: <span className=" text-black">{result.account}</span></p>
@@ -112,7 +131,7 @@ const ScanModal = () => {
         >
           Pay
         </button>
-      </div> */}
+      </div>
     </div>
     )
   }
