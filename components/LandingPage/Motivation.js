fbcd57b4fa087ca4e83aebd511fc7376ba336951
Adithya2310 <Adithyanandi099@gmail.com>
2023-10-21 10:47:05 +0530

    adde qrcode

--- components/LandingPage/Motivation.js
+++ components/LandingPage/Motivation.js
@@ -29,30 +29,29 @@ const motivationItems = [
 
 const Motivation = () => {
   return (
-        <section className="py-14 bg-neutral-light-grayish-blue lg:py-24">
+        <section className="bg-neutral-light-grayish-blue mb-32 ">
       <div className="container text-center lg:text-left">
-        <div className="grid lg:grid-cols-2 mb-12 lg:mb-16">
+        <div className="grid lg:grid-cols-2 lg:mb-16">
           <div className="col-span-1">
             <h2 className="text-3xl lg:text-4xl text-primary-dark-blue pb-5">
-              Why choose Easybank?
+              Why choose uPay
             </h2>
             <p className="text-neutral-grayish-blue text-sm lg:text-base leading-5">
-              We leverage Open Banking to turn your bank account into your
-              financial hub. Control your finances like never before.
+              We leverage Sui Protocol for merchants to make it easy to recieve the payments in Sui and make it a frictionless experience
             </p>
           </div>
         </div>
         <div className="grid grid-cols-1 gap-9 lg:gap-6 lg:grid-cols-4">
           {motivationItems.map((item) => (
-            <div key={item.title} className="justify-center">
-              <div className="flex justify-center lg:justify-start">
-                <img src={item.iconPath} alt="" />
+            <div key={item.title} className=" felx flex-col w-full  items-center justify-center">
+              <div className="flex  justify-center lg:justify-center">
+                <img className=' ' src={item.iconPath} alt="" />
               </div>
 
-              <h2 className="text-lg text-primary-dark-blue py-4 lg:pt-9 lg:pb-6 lg:text-xl lg:font-bold">
+              <h2 className="text-lg text-center text-primary-dark-blue py-4 lg:pt-9 lg:pb-6 lg:text-xl lg:font-bold">
                 {item.title}
               </h2>
-              <p className="text-neutral-grayish-blue text-sm font-light lg:text-base leading-5">
+              <p className="text-neutral-grayish-blue text-center text-sm font-light lg:text-base leading-5">
                 {item.subtitle}
               </p>
             </div>
--- components/LandingPage/Navbar.js
+++ components/LandingPage/Navbar.js
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="flex justify-between">
-        <div className="my-5 lg:my-6">
+        <div className="mt-5">
             <Image src="/images/logo.svg" height={32} width={160} alt="easybank logo" />
         </div>
-        <div className="h-[50px] ">
+        <div className=" ">
         <Link href="/home">
-        <button className="hidden h-full  lg:block bg-primary-lime-green rounded-full px-7  text-neutral-white text-xs bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan hover:button-brightness focus:outline-none focus:ring ring-green-400">
+        <button className="hidden h-full  lg:block bg-blue-500 rounded-full px-7  text-neutral-white text-xs from-primary-lime-green to-primary-bright-cyan hover:button-brightness focus:outline-none focus:ring ring-green-400">
             Launch App
         </button>
         </Link>
